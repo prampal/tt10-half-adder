91c56b7b5f8935fe7f3fb72bc2572f4db8bccca1
prampal <93888290+prampal@users.noreply.github.com>
2026-01-19 14:09:13 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,14 +1,21 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, Timer
+from cocotb.triggers import ClockCycles
+
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
+    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
+    # Reset
+    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -16,30 +23,33 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    # A=0, B=0 → S=0, C=0
+    dut._log.info("Test project behavior")
+
+    # Set the input values you want to test
     dut.ui_in[0].value = 0
     dut.ui_in[1].value = 0
-    await Timer(1, units="ns")
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    # Assert the actual expected output of your test case:
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 0
-
-    # A=0, B=1
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
+    # Repeat for the remaining 3 test cases
     dut.ui_in[0].value = 0
     dut.ui_in[1].value = 1
-    await Timer(1, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
-
-    # A=1, B=0
+    
     dut.ui_in[0].value = 1
     dut.ui_in[1].value = 0
-    await Timer(1, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
-
-    # A=1, B=1
+    
     dut.ui_in[0].value = 1
     dut.ui_in[1].value = 1
-    await Timer(1, units="ns")
+    await ClockCycles(dut.clk, 1)
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 1
