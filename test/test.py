f1c39c21277c07977d8ea7a5fa2495827e6b9756
prampal <93888290+prampal@users.noreply.github.com>
2026-01-19 14:03:47 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, ReadOnly
+from cocotb.triggers import ClockCycles, Timer
 
 @cocotb.test()
 async def test_project(dut):
@@ -9,7 +9,6 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
@@ -17,36 +16,30 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
     # A=0, B=0 → S=0, C=0
     dut.ui_in[0].value = 0
     dut.ui_in[1].value = 0
-    await ClockCycles(dut.clk, 1)
-    await ReadOnly()
+    await Timer(1, units="ns")
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 0
 
-    # A=0, B=1 → S=1, C=0
+    # A=0, B=1
     dut.ui_in[0].value = 0
     dut.ui_in[1].value = 1
-    await ClockCycles(dut.clk, 1)
-    await ReadOnly()
+    await Timer(1, units="ns")
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
 
-    # A=1, B=0 → S=1, C=0
+    # A=1, B=0
     dut.ui_in[0].value = 1
     dut.ui_in[1].value = 0
-    await ClockCycles(dut.clk, 1)
-    await ReadOnly()
+    await Timer(1, units="ns")
     assert dut.uo_out[0].value == 1
     assert dut.uo_out[1].value == 0
 
-    # A=1, B=1 → S=0, C=1
+    # A=1, B=1
     dut.ui_in[0].value = 1
     dut.ui_in[1].value = 1
-    await ClockCycles(dut.clk, 1)
-    await ReadOnly()
+    await Timer(1, units="ns")
     assert dut.uo_out[0].value == 0
     assert dut.uo_out[1].value == 1
